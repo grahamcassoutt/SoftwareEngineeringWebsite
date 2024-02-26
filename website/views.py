ba2a40265c2b8984f4c1252dd5969424521cc05b
grahamcassoutt <grahamc@jeveg.com>
2024-02-26 11:19:29 -0600

    initial commit

---
+++ website/views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    is_home_page = True
+    return render_template('home.html', is_home_page=is_home_page)
+
