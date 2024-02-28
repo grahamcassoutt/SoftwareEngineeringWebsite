d9ddca82f0af9d8414e5cd9b23673dc1d5102baf
grahamcassoutt <grahamc@jeveg.com>
2024-02-28 17:54:30 -0600

    address changes

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/personalProject')
 def home():
     is_home_page = True
     return render_template('home.html', is_home_page=is_home_page)
