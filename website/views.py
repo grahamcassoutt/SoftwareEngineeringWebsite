32d8dbabc0eae715f4b18f645c97d2e446fb01ca
grahamcassoutt <grahamc@jeveg.com>
2024-02-29 11:13:05 -0600

    get method

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route('/personalProject')
+@views.route('/personalProject', methods=['GET'])
 def home():
     is_home_page = True
     return render_template('home.html', is_home_page=is_home_page)
